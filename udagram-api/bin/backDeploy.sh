5fecc4b0846f1c9c73fe06154193d2fe0d58c888
Ibrahim Ahmed <ibrahimahmed90000@gmail.com>
2022-04-06 06:29:57 +0200

    Fix EB deploy 6

--- udagram-api/bin/backDeploy.sh
+++ udagram-api/bin/backDeploy.sh
@@ -1,1 +1,1 @@
-"cd C:\Users\DELL\Desktop\Hosting a Full Stack Application\udagram\udagram-api\www && eb deploy"
+cd C:\\Users\\DELL\\Desktop\\Hosting a Full Stack Application\\udagram\\udagram-api\\www && eb deploy
